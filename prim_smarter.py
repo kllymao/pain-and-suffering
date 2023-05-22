0a996c0801cd8658c98f0e639036c004e515b3f9
Van Olson <vanolson@uchicago.edu>
2023-05-21 22:41:03 -0500

    Working modified prims, removed extraneous description

--- prim_smarter.py
+++ prim_smarter.py
@@ -1,13 +1,5 @@
 """
-Teleporters
-
-Incoming Dean of the College, Melina Hale, has decided that winters have gotten too long and by next winter, the beloved tunnels connecting the entire campus should be accessible once again. Unfortunately, all the old tunnels running under the quad have become unusable and new ones need to be built. Of course, this should be kept economical and financially responsible. An engineering firm has provided all possible tunnels that could be dug, and of course the price tag for each of them. In a wild revelation, Fermilab announced that they now have a working teleporter, but it only works for short distances. The engineering firm has provided estimates for installing these teleporters in various building around campus (but not all). The dean wants all buildings to be connected by at least one path, but also for the entire project to cost as low as possible. To that end, you need to find the best set of tunnels and (maybe) teleporters to connect the entire campus for the next winter.
-
-Input: In the first line there are three integers $N$, $K$, $M$, which correspond to the number of buildings, the number of buildings where a teleporter can be installed and the number of possible tunnels between buildings.
-In the next $K$ lines there are two integers, $i$ and $B[i]$, meaning that a teleporter can be installed in building $i$ with cost $B[i]$.
-Finally, the next $M$ lines contain three integers $i$, $j$, $c[i, j]$, which denote that there is a proposed tunnel between buildings $i$ and $j$ with cost $c[i, j]$.
-
-Output: One line with a single integer, the minimum cost for the entire network.
+Prim's Algorithm for Bounded MSTs with some additional modifications
 """
 
 from collections import defaultdict
