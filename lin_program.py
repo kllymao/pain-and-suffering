c6cb852be36639ae8f9f7c5b87e6fef07b9e5995
Kelly Mao <49347089+kllymao@users.noreply.github.com>
2023-05-21 22:36:23 -0500

    Add files via upload

--- lin_program.py
+++ lin_program.py
@@ -10,75 +10,113 @@ import heapq
 import cvxpy as cp
 
 def solve(N, M, bounds, edges, edges_dict):
-    # CREATE LINEAR OPTIMIZATION PROBLEM!
 
-    # an array of optimization variables:
     # each optimization variable is 1 or 0 depending on if indexed edge is in mst or not
-    opts = cp.Variable(M, boolean=True)
+    opts = cp.Variable(shape=(M,1), boolean=True)
 
-    # constraints
+    # constraints:
     # 1. sum of the optimization variables = N - 1
-    # 2. for all edges in a subset S = (V', E') of G (for all subsets)
-    # (yikes! this is a LOT of subsets! might have to make it smaller in some way),
-    # make sure sum of optimization variables = |V'| - 1
+    # 2. this constraint would have made it exponentially slow so i'm getting rid of this lol
     # 3. for each vertex v in the graph, we make sure the sum of the optimization variables corresponding
     # to the edges attached to v is less than or equal to the bound of v
     # (4. optimization variables can only be 0 or 1. this is covered by the fact that opt variable is list of bools)
+    # 5. add a constraint each time we form a cycle or something idk
 
     constraints = [cp.sum(opts) == N-1] # 1st constraint
 
-    # 2nd constraint is: hard. see if this runs without it or if we can choose better subgraphs
-    # maybe choose a proportional number of random subgraphs and see if this property holds?
-
     for i in range(N): # 3rd constraint
-        bound = bounds[i]
-        i_edges = [] # at index j: 1 if edge j+1 connects to vertex i, 0 otherwise
-        for j in range(M):
-            if edges[j+1][0] == i or edges[j+1][1] == i:
-                i_edges.append(1)
-            else:
-                i_edges.append(0)
-        degree_vertex = i_edges @ opts
-        constraints.append(degree_vertex <= bound)
+        if bounds[i] != 1:
+            bound = bounds[i] - 1
+        else:
+            bound = bounds[i]
 
-    # add objective
-    # objective: for each edge in E, we add to our total weight the weight of that edge 
-    # multiplied by the optimization variable corresponding to that edge
-    # minimize this total weight
+    node_opts = [[] for _ in range(N + 1)]
+
+    for i, (u, v, w) in enumerate(edges):
+        node_opts[u].append(opts[i])
+        node_opts[v].append(opts[i])
+
+    for u in range(1, N + 1):
+        constraints.append(cp.sum(node_opts[u]) <= bounds[u - 1])
+        constraints.append(cp.sum(node_opts[u]) >= 1)
+
+    # add objective: minimize total weight
     edge_weights = [] # at index i, edge i+1 has some weight
     for i in range(M):
-        edge_weights.append(edges[i+1][2])
+        edge_weights.append(edges[i][2])
     total_weight = edge_weights @ opts
     objective = cp.Minimize(total_weight)
+
+    problem = cp.Problem(objective, constraints)
+    problem.solve()
+
+    # # now find connected components
+    # mst_edges = []
+    # for i in range(M):
+    #     if opts.value[i] == 1.0:
+    #         mst_edges.append(edges[i])
     
+    # mst_edges_dict = defaultdict(list)
+    # for edge in mst_edges:
+    #     u, v, weight = edge[0], edge[1], edge[2]
+    #     mst_edges_dict[u].append((v, weight))
+    #     mst_edges_dict[v].append((u, weight))
 
-    # SOLVE LINEAR OPTIMIZATION PROBLEM ITERATIVELY!
+    # unvisited = {}
+    # for i in range(1, N+1):
+    #     unvisited[i] = False
+    # visited = {}
 
-    return 0
-        
+    # while len(unvisited) != 0:
+    #     unvisited = explore(next(iter(unvisited)), mst_edges_dict, unvisited, visited)
+    #     unvisited_vertex = 0
+    #     if len(unvisited) == 0:
+    #         break
 
+    #     for vertex in unvisited:
+    #             unvisited_vertex = vertex
 
-def read_input():
+    #     nodes_indices = []
+    #     if unvisited_vertex != 0:
+    #         for (v2, index) in edges_dict[unvisited_vertex]:
+    #             if v2 in visited:
+    #                 nodes_indices.append(index)
+    #                 print(nodes_indices)
+
+    #         constraints.append(cp.sum([opts[i] for i in nodes_indices]) >= 1)
+    #         # add a constraint that there should be an edge from unvisited to any edge in edges reachable from initial vertex
+
+    #         problem = cp.Problem(objective, constraints)
+    #         problem.solve()
+    
+    for i in range(M):
+        if opts.value[i] == 1.0:
+            print(i+1)
+
+# def explore(vertex, mst_edges_dict, unvisited, visited):
+#     del unvisited[vertex]
+#     visited[vertex] = True
+#     for (v2, weight) in mst_edges_dict[vertex]:
+#         if v2 in unvisited:
+#             explore(v2, mst_edges_dict, unvisited, visited) 
+#     return unvisited
+
+def read_input(): # note: at index i we're looking at edge or vertex i+1
     N, M = [int(i) for i in input().split()]
     bounds = [int(input()) for _ in range(N)]
-    bounds.insert(0, None)
     edges = [[int(i) for i in input().split()] for _ in range(M)]
-    edges.insert(0, None)
 
     edges_dict = defaultdict(list)
     for i in range(M):
-        u, v, c = edges[i+1][0], edges[i+1][1], edges[i+1][2]
-        edges_dict[u].append((v, c))
-        edges_dict[v].append((u, c))
-
-    print(bounds, edges)
+        u, v, c = edges[i][0], edges[i][1], edges[i][2]
+        edges_dict[u].append((v, i+1))
+        edges_dict[v].append((u, i+1))
 
     return N, M, bounds, edges, edges_dict
 
 def main():
     N, M, bounds, edges, edges_dict = read_input()
-    total_cost = solve(N, M, bounds, edges, edges_dict)
-    print(total_cost)
+    solve(N, M, bounds, edges, edges_dict)
 
 if __name__ == '__main__':
     main()
